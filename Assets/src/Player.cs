fbc2804a89d9bdec0d04d9594ae6127df8d5e706
Ankur Banerjee <rukna2003@gmail.com>
2026-07-28 20:13:05 +0530

    player movement logic done

--- Assets/src/Player.cs
+++ Assets/src/Player.cs
@@ -2,20 +2,60 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     
+    
+
+
+
     Rigidbody2D rb;
 
     public float move_speed = 6f;
+    public float jump_speed = 10f;
+
+    bool is_grounded;
+
+
+
+
+
 
     void Start() {
-
         rb = GetComponent<Rigidbody2D>();
-
     }
 
     void Update() {
+        handle_input();
+    }
 
+
+
+
+
+
+    void handle_input() {
+
+        // left and right
         rb.linearVelocity = new Vector2(Input.GetAxisRaw("Horizontal") * move_speed, rb.linearVelocity.y);
 
+        // jump
+        if (Input.GetButtonDown("Jump") && is_grounded) {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jump_speed);
+        }
+
+    }
+
+
+
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            is_grounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            is_grounded = false;
+        }
     }
 
 }
